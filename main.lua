045c25f938d1ed5ce3c02f0aa1e5ffa716c33e02
nightstalker6669 <nightstalker6669@yahoo.com>
2023-12-22 14:52:21 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -10,13 +10,6 @@ local function main()
     
     -- Check and craft items if necessary
     inventoryManager.checkAndCraftItems()
-
-    -- Get colony data
-    local colonyData = ColonyIntegrator.getColonyData(integrator)
-
-    -- Do something with the colony data, e.g., print it to the terminal for now
-    print(textutils.serialize(colonyData))
-    
     -- Call the main application which will now handle user interface rendering
     application.main()
 end
