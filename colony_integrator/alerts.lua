ffea51dc0763d57bbe4168ab656af0156985fd84
nightstalker6669 <nightstalker6669@yahoo.com>
2023-12-22 21:53:08 -0500

    1

--- colony_integrator/alerts.lua
+++ colony_integrator/alerts.lua
@@ -6,7 +6,6 @@ function Alerts.getColonyAlerts(integratorPeripheral)
     if not integratorPeripheral then
         error("Integrator peripheral is required")
     end
-    -- [INPUT_REQUIRED] Use actual method to retrieve alerts if different from 'getColonyAlerts'
     local alerts = integratorPeripheral.isUnderAttack()
     return alerts
 end
